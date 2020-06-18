88aa30c8ace2908215b2961b46730ae185313fab
chandan-pal <chandanpal290190@gmail.com>
2020-06-18 17:31:53 +0530

    initialize spring boot project with web jpa jdbc mysql driver and spring security dependency

---
+++ src/main/java/in/chandanpal/loginregisterjwtmysql/LoginRegisterJwtMysqlApplication.java
@@ -0,0 +1,13 @@
+package in.chandanpal.loginregisterjwtmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginRegisterJwtMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginRegisterJwtMysqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/chandanpal/loginregisterjwtmysql/LoginRegisterJwtMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package in.chandanpal.loginregisterjwtmysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginRegisterJwtMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
