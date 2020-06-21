ad711826f30d3d1ae6ed28c4ff4512aca83c8781
chandan-pal <chandanpal290190@gmail.com>
2020-06-21 18:13:52 +0530

    allow cross origin request and send json response

--- src/main/java/in/chandanpal/loginregisterjwtmysql/service/CommonServices.java
+++ src/main/java/in/chandanpal/loginregisterjwtmysql/service/CommonServices.java
@@ -1,14 +1,22 @@
 package in.chandanpal.loginregisterjwtmysql.service;
 
+import java.util.HashMap;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 public class CommonServices
 {
     @GetMapping("/test")
-    public String testAuthentication() {
-        return "You are authenticated!";
+    public ResponseEntity<?> testAuthentication() {
+        HashMap<String, String> responseMap = new HashMap<>();
+        responseMap.put("message", "You are Authenticated!");
+        return ResponseEntity.ok(responseMap);
     }
 
 }
